daf84e7f4e7c2516cbb3a035ba5bc8dd0508b801
Valeri Filipov <v.filipov87@gmail.com>
2016-09-17 14:58:48 +0200

    Age Calculator

---
+++ 01 Homework Projects/Solution1/CalculateBirthday/CalculateBirthday.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateBirthday
+{
+    class CalculateBirthday
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter your birtdate: ");
+                    DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+                    int Days = (DateTime.Now.Year * 365 + DateTime.Now.DayOfYear) - (birthDate.Year * 365 + birthDate.DayOfYear);
+                    int Years = Days / 365;
+                    string message = (Days >= 365) ? "Your age: " + Years + " years" : "Your age: " + Days + " days";
+
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("You have entered an invalid date.\n");
+                }
+
+                Console.WriteLine("Exit? (y/n)");
+                string userValue = Console.ReadLine();
+
+                if (userValue == "y")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
