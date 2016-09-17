57948ea5219971464d5c1229ee71f619021c25d6
Valeri Filipov <v.filipov87@gmail.com>
2016-09-17 14:02:26 +0200

    Assignment 9: Print Digit Sequence

---
+++ 01 Homework Projects/Solution1/PrintDigitSequence/PrintDigitalSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class PrintDigitalSequence
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i=2; i < 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                Console.WriteLine(i);
+                }
+                else
+                {
+                Console.WriteLine(-i);
+                }
+            }
+        }
+    }
+
