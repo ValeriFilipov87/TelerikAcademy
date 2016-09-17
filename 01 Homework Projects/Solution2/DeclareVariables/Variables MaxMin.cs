e00202c8db8b23b610ea84929650ab9957d32a67
Valeri Filipov <v.filipov87@gmail.com>
2016-09-17 22:53:08 +0200

    Fixed Comments and Class name

--- 01 Homework Projects/Solution2/DeclareVariables/Variables MaxMin.cs
+++ 01 Homework Projects/Solution2/DeclareVariables/Variables MaxMin.cs
@@ -1,11 +1,6 @@
-﻿/* Declare five variables choosing for each of them the most appropriate of the types byte, sbyte, short, ushort, int, uint, long, ulong to 
-represent the following values: 52130, -115, 4825932, 97, -10000. Choose a large enough type for each number to ensure it will fit in it. 
-Try to compile the code.
-*/
+﻿using System;
 
-using System;
-
-    class DeclareVariables
+    class VariablesMaxMin
     {
         static void Main()
         {
