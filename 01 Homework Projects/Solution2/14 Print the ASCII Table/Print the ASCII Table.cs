252c3c36c4795005736bd9e62ee4965e745829d4
Valeri Filipov <v.filipov87@gmail.com>
2016-09-20 22:46:50 +0200

    14 Print the ASCII Table

---
+++ 01 Homework Projects/Solution2/14 Print the ASCII Table/Print the ASCII Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+    class PrintTheASCIITable
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.ASCII;
+            for(int i = 31; i<128; i++)
+            {
+                char x = (char)i;
+                Console.WriteLine(x);
+            }
+        }
+    }
+
