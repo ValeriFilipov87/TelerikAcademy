006a8a9275044eefc98b935c7bd9e4cd02d6b6b7
Valeri Filipov <v.filipov87@gmail.com>
2016-09-20 21:09:59 +0200

    13 Comparing Floats

---
+++ 01 Homework Projects/Solution2/13 Comparing Floats/Comparing Floats.cs
@@ -0,0 +1,23 @@
+﻿/* Write a program that safely compares floating-point numbers (double) with precision eps = 0.000001. */
+
+using System;
+
+class ComparingFloats
+{
+        static void Main()
+        {
+            Double eps = 0.000000000001;
+            Console.WriteLine("Write your first floating number");
+            float firstNumber = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write your second floating number");
+            float secondNumber = (float)Convert.ToDouble(Console.ReadLine());
+            if (Math.Abs(firstNumber - secondNumber) < eps)
+            {
+                Console.WriteLine("A > B");
+            }
+            else
+            {
+                Console.WriteLine("B > A");
+            }
+        }
+}
