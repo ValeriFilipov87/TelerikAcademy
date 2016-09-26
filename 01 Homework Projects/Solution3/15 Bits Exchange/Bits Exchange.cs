2f18cc18efe2dacfc8869abfa75ea7feed4376d5
Valeri Filipov <v.filipov87@gmail.com>
2016-09-26 23:52:40 +0200

    15 Bit exchange.. it cannot be compiled

---
+++ 01 Homework Projects/Solution3/15 Bits Exchange/Bits Exchange.cs
@@ -0,0 +1,149 @@
+﻿/*  Write a program that exchanges bits 3, 4 and 5 with bits 24, 25 and 26 of given 32-bit unsigned integer. */
+
+using System;
+
+class BitsExchange
+{
+    static void Main()
+    {
+        //insert the integer number which should be modified
+        Console.WriteLine("Input an unsigned integer:");
+        double digit = double.Parse(Console.ReadLine());
+        double digit1 = Math.Ceiling(digit);
+        bool checkNumber = (digit == digit1) && (digit >= 0); // check if the 1st input digit is: 1. Intiger; 2. Positive 
+        while (checkNumber == false) // cycle which will check each input digit after the first one which is wrong, until a correct digit is input
+        {
+            Console.Clear();
+            Console.WriteLine("The input number is not a unsigned intiger! Please write a unsigned intiger number:");
+            digit = double.Parse(Console.ReadLine());
+            digit1 = Math.Ceiling(digit);
+            checkNumber = (digit == digit1) && (digit >= 0); // check if the next input digit is: 1. Intiger; 2. Positive 
+        }
+
+        int n = Convert.ToInt32(digit);         // the input number number
+        Console.WriteLine("The input value is:{0}\nThe input value in duoble is:{1}", n, Convert.ToString(n, 2).PadLeft(32, '0'));
+
+        Console.WriteLine(n);
+        Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+
+        int mask;
+        mask = 1 << 3;
+        int thirdBit = (n & mask) >> 3;
+        mask = 1 << 4;
+        int forthBit = (n & mask) >> 4;
+        mask = 1 << 5;
+        int fifthBit = (n & mask) >> 5;
+        mask = 1 << 24;
+        int twForthBit = (n & mask) >> 24;
+        mask = 1 << 25;
+        int twFifthBit = (n & mask) >> 25;
+        mask = 1 << 26;
+        int twSixthBit = (n & mask) >> 26;
+
+        bool checkFirstPair = thirdBit != twForthBit;
+        bool checkSecondPair = forthBit != twFifthBit;
+        bool checkThirdPair = fifthBit != twSixthBit;
+
+        int result;
+
+        if (checkFirstPair)
+        {
+            if (thirdBit == 1)
+            {
+                mask = ~(1 << 3);
+                result = n & mask;
+            }
+            else
+            {
+                mask = 1 << 3;
+                result = n | mask;
+            }
+            Console.WriteLine("{0}", result);
+            Console.WriteLine(Convert.ToString(result, 2).PadLeft(32, '0'));
+
+            if (twForthBit == 1)
+            {
+                mask = ~(1 << 24);
+                result = result & mask;
+            }
+            else
+            {
+                mask = 1 << 24;
+                result = result | mask;
+            }
+            Console.WriteLine("{0}", result);
+            Console.WriteLine(Convert.ToString(result, 2).PadLeft(32, '0'));
+        }
+        
+        if (checkSecondPair)
+        {
+            if (forthBit == 1)
+            {
+                mask = ~(1 << 4);
+                result = result & mask;
+            }
+            else
+            {
+                mask = 1 << 4;
+                result = result | mask;
+            }
+            if (twFifthBit == 1)
+            {
+                mask = ~(1 << 25);
+                result = result & mask;
+            }
+            else
+            {
+                mask = 1 << 25;
+                result = result | mask;
+            }
+            return;
+        }
+        else
+        if (checkThirdPair)
+        {
+            if (fifthBit == 1)
+            {
+                mask = ~(1 << 5);
+                result = result & mask;
+            }
+            else
+            {
+                mask = 1 << 5;
+                result = result | mask;
+            }
+            if (twSixthBit == 1)
+            {
+                mask = ~(1 << 26);
+                result = result & mask;
+            }
+            else
+            {
+                mask = 1 << 26;
+                result = result | mask;
+            }
+            return;
+        }
+        Console.WriteLine("{0}",result);
+        Console.WriteLine(Convert.ToString(result, 2).PadLeft(32, '0'));
+    }
+}
+
+/*ChangeBit(int n, int v, int p)
+{
+    int value = v;
+    int place = p;
+    int mask;
+    int result = n;
+    if (value == 1)
+    {
+        mask = ~(1 << p);
+        result = n & mask;
+        return;
+    }
+    else
+    {
+        mask = 1 << p;
+        return result = n | mask;
+    }
+}*/
