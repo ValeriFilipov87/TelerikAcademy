a828f756187a5629a76d9fb185150b4b23d25ddb
Valeri Filipov <v.filipov87@gmail.com>
2016-10-02 14:07:55 +0200

    updated list with variables values

--- 01 Homework Projects/Solution2/Variables MaxMin/Variables MaxMin.cs
+++ 01 Homework Projects/Solution2/Variables MaxMin/Variables MaxMin.cs
@@ -1,51 +1,55 @@
 ﻿using System;
 
-    class VariablesMaxMin
+class VariablesMaxMin
+{
+    static void Main()
     {
-        static void Main()
-        {
-            byte maxbyte = byte.MaxValue;
-            byte minbyte = byte.MinValue;
-            sbyte maxsbyte = sbyte.MaxValue;
-            sbyte minsbyte = sbyte.MinValue;
-            short maxshort = short.MaxValue;
-            short minshort = short.MinValue;
-            ushort maxushort = ushort.MaxValue;
-            ushort minushort = ushort.MinValue;
-            int maxint = int.MaxValue;
-            int minint = int.MinValue;
-            uint maxuint = uint.MaxValue;
-            uint minuint = uint.MinValue;
-            long maxlong = long.MaxValue;
-            long minlong = long.MinValue;
-            ulong maxulong = ulong.MaxValue;
-            ulong minulong = ulong.MinValue;
-            UInt16 maxUint16 = UInt16.MaxValue;
-            UInt32 maxUint32 = UInt32.MaxValue;
-            UInt64 maxUint64 = UInt64.MaxValue;
+        byte maxbyte = byte.MaxValue;
+        byte minbyte = byte.MinValue;
+        sbyte maxsbyte = sbyte.MaxValue;
+        sbyte minsbyte = sbyte.MinValue;
+        short maxshort = short.MaxValue;
+        short minshort = short.MinValue;
+        ushort maxushort = ushort.MaxValue;
+        ushort minushort = ushort.MinValue;
+        decimal maxdecimal = Decimal.MaxValue;
+        decimal mindecimal = Decimal.MinValue;
+        int maxint = int.MaxValue;
+        int minint = int.MinValue;
+        uint maxuint = uint.MaxValue;
+        uint minuint = uint.MinValue;
+        long maxlong = long.MaxValue;
+        long minlong = long.MinValue;
+        ulong maxulong = ulong.MaxValue;
+        ulong minulong = ulong.MinValue;
+        UInt16 maxUint16 = UInt16.MaxValue;
+        UInt32 maxUint32 = UInt32.MaxValue;
+        UInt64 maxUint64 = UInt64.MaxValue;
 
-            Console.WriteLine("byte max = {0}", maxbyte);
-            Console.WriteLine("byte min = {0}", minbyte);
-            Console.WriteLine("sbyte max = {0}", maxsbyte);
-            Console.WriteLine("sbyte min = {0}", minsbyte);
-            Console.WriteLine("short max = {0}", maxshort);
-            Console.WriteLine("short min = {0}", minshort);
-            Console.WriteLine("ushort max = {0}", maxushort);
-            Console.WriteLine("ushort min = {0}", minushort);
-            Console.WriteLine("int max = {0}", maxint);
-            Console.WriteLine("int min = {0}", minint);
-            Console.WriteLine("uint max = {0}", maxuint);
-            Console.WriteLine("uint min = {0}", minuint);
-            Console.WriteLine("long max = {0}", maxlong);
-            Console.WriteLine("long min = {0}", minlong);
-            Console.WriteLine("ulong max = {0}", maxulong);
-            Console.WriteLine("ulong min = {0}", minulong);
-            Console.WriteLine("Uint16 max = {0}", maxUint16);
-            Console.WriteLine("Uint32 max = {0}", maxUint32);
-            Console.WriteLine("Uint64 max = {0}", maxUint64);
-  
+        Console.WriteLine("byte max = {0}", maxbyte);
+        Console.WriteLine("byte min = {0}", minbyte);
+        Console.WriteLine("sbyte max = {0}", maxsbyte);
+        Console.WriteLine("sbyte min = {0}", minsbyte);
+        Console.WriteLine("short max = {0}", maxshort);
+        Console.WriteLine("short min = {0}", minshort);
+        Console.WriteLine("ushort max = {0}", maxushort);
+        Console.WriteLine("ushort min = {0}", minushort);
+        Console.WriteLine("Decimal max = {0}", maxdecimal);
+        Console.WriteLine("Decimal min = {0}", mindecimal);
+        Console.WriteLine("int max = {0}", maxint);
+        Console.WriteLine("int min = {0}", minint);
+        Console.WriteLine("uint max = {0}", maxuint);
+        Console.WriteLine("uint min = {0}", minuint);
+        Console.WriteLine("long max = {0}", maxlong);
+        Console.WriteLine("long min = {0}", minlong);
+        Console.WriteLine("ulong max = {0}", maxulong);
+        Console.WriteLine("ulong min = {0}", minulong);
+        Console.WriteLine("Uint16 max = {0}", maxUint16);
+        Console.WriteLine("Uint32 max = {0}", maxUint32);
+        Console.WriteLine("Uint64 max = {0}", maxUint64);
 
-        }    
 
     }
+
+}
 
---
+++ 01 Homework Projects/Solution4/01 Sum of 3 Numbers/Sum of 3 Numbers.cs
@@ -0,0 +1,12 @@
+﻿/* Write a program that reads 3 real numbers from the console and prints their sum. */
+
+using System;
+
+class ProblemSumOfThreeNumbers
+{
+    static void Main()
+    {
+
+    }
+}
+
