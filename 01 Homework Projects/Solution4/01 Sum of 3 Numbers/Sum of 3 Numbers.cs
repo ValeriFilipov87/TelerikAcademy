e6a25e635cc36cb89945e4337e378072e6c52907
Valeri Filipov <v.filipov87@gmail.com>
2016-10-02 14:21:01 +0200

    Problem 1. Sum of 3 Numbers

--- 01 Homework Projects/Solution4/01 Sum of 3 Numbers/Sum of 3 Numbers.cs
+++ 01 Homework Projects/Solution4/01 Sum of 3 Numbers/Sum of 3 Numbers.cs
@@ -2,11 +2,19 @@
 
 using System;
 
+
 class ProblemSumOfThreeNumbers
 {
     static void Main()
     {
+        Console.WriteLine("Please input first number");
+        decimal firstNumber = Decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Please input second number");
+        decimal secondNumber = Decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Please input third number");
+        decimal thirdNumber = Decimal.Parse(Console.ReadLine());
 
+        Console.WriteLine("The sum of the three input numbers is {0}", firstNumber + secondNumber + thirdNumber);
     }
 }
 
