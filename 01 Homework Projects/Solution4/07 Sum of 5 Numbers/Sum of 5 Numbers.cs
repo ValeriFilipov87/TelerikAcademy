d44de60ec58ee12ef332b6e1c2b3c23e363db75f
Valeri Filipov <v.filipov87@gmail.com>
2016-10-03 22:05:56 +0200

    Problem 7. Sum of 5 Numbers

---
+++ 01 Homework Projects/Solution4/07 Sum of 5 Numbers/Sum of 5 Numbers.cs
@@ -0,0 +1,31 @@
+﻿/*  Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum. */
+
+using System;
+
+class SumOfFiveNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input 5 digits:");
+        string inputNumber = Console.ReadLine();
+        int length = 0;
+        var array = inputNumber.Split(' ');
+        if (inputNumber[inputNumber.Length - 1] == ' ')
+        {
+            length = array.Length - 1;
+        }
+        else
+        {
+            length = array.Length;
+        }
+        decimal y = new decimal();
+        decimal result = new decimal();
+        for (int i = 0; i < length; i++)
+        {
+            y = decimal.Parse(array[i]);
+            result = y + result;
+        }
+        Console.WriteLine(result);
+
+    }
+}
