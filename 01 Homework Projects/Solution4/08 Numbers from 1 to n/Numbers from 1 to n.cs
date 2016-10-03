88874c38a4ca32c391880504e2f419b38bbdeb9b
Valeri Filipov <v.filipov87@gmail.com>
2016-10-03 22:17:28 +0200

    Problem 8. Numbers from 1 to n

---
+++ 01 Homework Projects/Solution4/08 Numbers from 1 to n/Numbers from 1 to n.cs
@@ -0,0 +1,27 @@
+﻿/*  Write a program that reads an integer number n from the console and prints all the numbers in the interval [1..n], each on a single line.
+    Note: You may need to use a for-loop. */
+
+using System;
+
+class NumbersFromOneToN
+{
+    static void Main()
+    {
+        Console.WriteLine("Please insert an intiger number:");
+        int number;
+        bool isParsed = int.TryParse(Console.ReadLine(), out number);
+        Console.Clear();
+        while (isParsed = !true || number <= 0)
+        {
+            Console.Clear();
+            Console.WriteLine("Wrong input!");
+            Console.WriteLine("Please insert positive integer:");
+            isParsed = int.TryParse(Console.ReadLine(), out number);
+        }
+        for (int i = 1; i <= number; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
