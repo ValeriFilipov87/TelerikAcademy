d257219a4412ab9b3c3a600ebd169320fbe71666
Valeri Filipov <v.filipov87@gmail.com>
2016-10-03 22:30:13 +0200

    Problem 9. Sum of n Numbers

---
+++ 01 Homework Projects/Solution4/09 Sum of n Numbers/Sum of n Numbers.cs
@@ -0,0 +1,34 @@
+﻿/* Write a program that enters a number n and after that enters more n numbers and calculates and prints their sum. Note: You may need to use a for-loop. */
+
+using System;
+
+class SumOfNNumbers
+{
+    static void Main()
+    {
+        int number;
+        decimal result = 0;
+        decimal additionalNumbers = 0;
+
+        Console.WriteLine("Please insert an intiger number:");
+        bool isParsed = int.TryParse(Console.ReadLine(), out number);
+
+        Console.Clear();
+
+        while (isParsed = !true || number <= 0)
+        {
+            Console.Clear();
+            Console.WriteLine("Wrong input!");
+            Console.WriteLine("Please insert positive integer:");
+            isParsed = int.TryParse(Console.ReadLine(), out number);
+        }
+
+        for (int i = 0; i < number; i++)
+        {
+            additionalNumbers = decimal.Parse(Console.ReadLine());
+            result = result + additionalNumbers;
+        }
+        Console.WriteLine(result);
+    }
+}
+
