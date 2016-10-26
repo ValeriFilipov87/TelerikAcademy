4bb42bc5b5aaa09a8a6a47ae25be27fc9cde40ad
Valeri Filipov <v.filipov87@gmail.com>
2016-10-26 23:43:20 +0200

    special for bace Kiko.. tail recursion and recursion

---
+++ 01 Homework Projects/Solution6/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Special for bace Kiko */
+
+using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static int acc = 1;
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            long result = factorial(n,acc);
+            Console.WriteLine(result);
+        }
+
+        private static long factorial(int n, int acc)
+        {
+
+            /*if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }*/
+            if (n == 0)
+            {
+                return acc;
+            }
+            else
+            {
+                return factorial(n - 1, acc * n);
+            }
+
+        }
+    }
+}
