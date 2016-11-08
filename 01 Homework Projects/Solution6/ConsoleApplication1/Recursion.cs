d4a3b57229b44738f71c0d994e83e40b515bde97
Valeri Filipov <v.filipov87@gmail.com>
2016-11-08 08:22:11 +0100

    fixed kiko's program

--- 01 Homework Projects/Solution6/ConsoleApplication1/Recursion.cs
+++ 01 Homework Projects/Solution6/ConsoleApplication1/Recursion.cs
@@ -1,40 +1,39 @@
 ﻿/* Special for bace Kiko */
 
 using System;
+using System.Numerics;
 
-
-namespace ConsoleApplication1
+class Recursion
 {
-    class Recursion
+    public static BigInteger acc = 1;
+    static void Main()
     {
-        public static int acc = 1;
-        static void Main()
+        int n = int.Parse(Console.ReadLine());
+        var result = new BigInteger();
+        result = factorial(n, acc);
+        Console.WriteLine(result);
+    }
+
+    private static BigInteger factorial(int n, BigInteger acc)
+    {
+
+        /*if (n == 0)
         {
-            int n = int.Parse(Console.ReadLine());
-            long result = factorial(n,acc);
-            Console.WriteLine(result);
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n - 1);
+        }*/
+        if (n == 0)
+        {
+            return acc;
+        }
+        else
+        {
+            return factorial(n - 1, acc * n);
         }
 
-        private static long factorial(int n, int acc)
-        {
-
-            /*if (n == 0)
-            {
-                return 1;
-            }
-            else
-            {
-                return n * factorial(n - 1);
-            }*/
-            if (n == 0)
-            {
-                return acc;
-            }
-            else
-            {
-                return factorial(n - 1, acc * n);
-            }
-
-        }
     }
 }
+
