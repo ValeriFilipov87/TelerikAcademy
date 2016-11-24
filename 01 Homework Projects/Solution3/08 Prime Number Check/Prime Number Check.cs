690725973cd97ea55ace586ae62e56a38b0c5485
Valeri Filipov <v.filipov87@gmail.com>
2016-11-24 20:00:22 +0100

    passes bg codder

--- 01 Homework Projects/Solution3/08 Prime Number Check/Prime Number Check.cs
+++ 01 Homework Projects/Solution3/08 Prime Number Check/Prime Number Check.cs
@@ -7,36 +7,25 @@ class PrimeNumberCheck
 {
     static void Main()
     {
-        Console.WriteLine("You must enter a positive intiger number");
-        double digit = double.Parse(Console.ReadLine());
-        double digit1 = Math.Ceiling(digit);
-        bool check = (digit == digit1) && (digit >= 0) && (digit <= 100); // check if the 1st input digit is: 1. Intiger; 2. Positive 3. Lesser from or even to 100
 
-        while (check == false) // cycle which will check each input digit after the first one which is wrong, until a correct digit is input
-        {
-            Console.Clear();
-            Console.WriteLine("The input number is not a positive intiger! Please write a positive intiger number");
-            digit = double.Parse(Console.ReadLine());
-            digit1 = Math.Ceiling(digit);
-            check = (digit == digit1) && (digit >= 0) && (digit <= 100); // check if the next input digit is: 1. Intiger; 2. Positive 3. Lesser from or even to 100
-        }
+        int digit = int.Parse(Console.ReadLine());
 
         bool prime = false;
-        for (int i = 2; i <= Math.Sqrt(digit); i++) // algorithm for finding a prime number: Devide the input number on every intiger number <= to the sqrt value of the input number
-        {                                           // then compare the remainder with 0
+        for (int i = 2; i <= Math.Sqrt(digit); i++)
+        {
             if (digit % i == 0)
             {
                 prime = true;
                 break;
             }
         }
-        if(prime)
+        if (prime || digit < 0)
         {
-            Console.WriteLine("The digit is not a prime number");
+            Console.WriteLine("false");
         }
         else
         {
-            Console.WriteLine("The digit is a prime number");
+            Console.WriteLine("true");
         }
     }
 }
