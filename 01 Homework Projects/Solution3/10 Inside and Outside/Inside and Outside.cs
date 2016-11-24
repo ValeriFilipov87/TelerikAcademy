981b58d75fdd756727e14ecda75c74992e6e759e
Valeri Filipov <v.filipov87@gmail.com>
2016-11-24 20:13:18 +0100

    pass bg codder

--- 01 Homework Projects/Solution3/10 Inside and Outside/Inside and Outside.cs
+++ 01 Homework Projects/Solution3/10 Inside and Outside/Inside and Outside.cs
@@ -6,43 +6,41 @@ class InsideAndOutside
 {
     static void Main()
     {
-        Console.WriteLine("You must enter x coordinate of a point");
+        //Console.WriteLine("You must enter x coordinate of a point");
         double xCoordinate = double.Parse(Console.ReadLine());
-        Console.WriteLine("You must enter y coordinate of a point");
+        //Console.WriteLine("You must enter y coordinate of a point");
         double yCoordinate = double.Parse(Console.ReadLine());
 
-        double hypothenuse = Math.Pow((xCoordinate-1), 2) + Math.Pow(yCoordinate-1, 2);
+        double hypothenuse = Math.Pow((xCoordinate - 1), 2) + Math.Pow(yCoordinate - 1, 2);
         double R = 1.5;
-        bool checkCircle = Math.Sqrt(hypothenuse) <= R;
 
-        if (checkCircle)
-        {
-            Console.WriteLine("The given point is inside the circle");
-        }
-        else
-        {
-            Console.WriteLine("The given point is not inside the circle");
-        }
+        bool checkCircle = Math.Sqrt(hypothenuse) <= R;
 
         bool checkRectangle = xCoordinate >= -1 && xCoordinate <= 5 && yCoordinate <= 1 && yCoordinate >= -1;
 
-        if (checkRectangle)
+        if (checkCircle == true)
         {
-            Console.WriteLine("The given point is inside the rectangle");
+            if (checkRectangle == true)
+            {
+                Console.WriteLine("inside circle inside rectangle");
+            }
+            else
+            {
+                Console.WriteLine("inside circle outside rectangle");
+            }
         }
         else
         {
-            Console.WriteLine("The given point is not inside the rectangle");
+            if (checkRectangle == true)
+            {
+                Console.WriteLine("outside circle inside rectangle");
+            }
+            else
+            {
+                Console.WriteLine("outside circle outside rectangle");
+            }
         }
 
-        if (checkCircle == true && checkRectangle == false)
-        {
-            Console.WriteLine("The condition is fullfilled");
-        }
-        else
-        {
-            Console.WriteLine("The condition is not fullfilled");
-        }
     }
 }
 
