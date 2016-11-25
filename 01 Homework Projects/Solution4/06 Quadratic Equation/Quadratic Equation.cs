e14e8b50657aa8d86049b7f95d4ec70990f15694
Valeri Filipov <v.filipov87@gmail.com>
2016-11-25 18:25:28 +0100

    pass bg codder

--- 01 Homework Projects/Solution4/06 Quadratic Equation/Quadratic Equation.cs
+++ 01 Homework Projects/Solution4/06 Quadratic Equation/Quadratic Equation.cs
@@ -6,24 +6,33 @@ class QuadraticEquation
 {
     static void Main()
     {
-        Console.WriteLine("Input coefficients for a,b and c:");
-        Console.WriteLine("ax^2 + bx+ c = 0");
-        Console.Write("a = ");
+       
         double a = double.Parse(Console.ReadLine());
-        Console.Write("b = ");
+     
         double b = double.Parse(Console.ReadLine());
-        Console.Write("c = ");
+      
         double c = double.Parse(Console.ReadLine());
         double descriminant = (b * b) - 4 * (a * c);
         if (descriminant < 0)
         {
-            Console.WriteLine("No real roots");
+            Console.WriteLine("no real roots");
         }
         else
         {
             double x1 = (-1 * b - Math.Sqrt(descriminant)) / (2 * a);
             double x2 = (-1 * b + Math.Sqrt(descriminant)) / (2 * a);
-            Console.WriteLine("x1 = {0}; x2 = {1}", x1, x2);
+            if (x1 == x2)
+            {
+                Console.WriteLine("{0:F2}", x1);
+            }
+            else if (x1 > x2)
+            {
+                Console.WriteLine("{0:F2}\n{1:F2}", x2, x1);
+            }
+            else
+            {
+                Console.WriteLine("{0:F2}\n{1:F2}", x1, x2);
+            }
         }
     }
 }
