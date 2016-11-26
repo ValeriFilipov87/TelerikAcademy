cabc548d0ce37df95552188936d5a6b919fe8f7d
Valeri Filipov <v.filipov87@gmail.com>
2016-11-26 16:04:33 +0100

    pass bg codder

--- 01 Homework Projects/Solution6/01 Numbers from 1 to N/Numbers from 1 to N.cs
+++ 01 Homework Projects/Solution6/01 Numbers from 1 to N/Numbers from 1 to N.cs
@@ -9,44 +9,16 @@ class NumbersFromOneToN
 {
     static void Main()
     {
-        int input = UserInput();
-        Console.WriteLine();
-        FirstSolution(input);
-        Console.WriteLine("\n");
-        SeondSolution(input);
-        Console.WriteLine();
-    }
-
-    private static void SeondSolution(int input)
-    {
-        for (int i = 1; i <= input; i++) // Second solution
+        int input = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= input; i++)
         {
-            Console.Write(i + " ");
+            if (i != 1 && i != input+1)
+            {
+                Console.Write(' ');
+            }
+            Console.Write(i);
         }
         Console.WriteLine();
-    }
-
-    private static void FirstSolution(int input)
-    {
-        int start = 1;
-        while (start <= input) // First solution
-        {
-            Console.Write(start + " ");
-            start++;
-        }
-    }
-
-    private static int UserInput()
-    {
-        int input = new int();
-        Console.WriteLine("Please input intiger number!");
-        bool check = int.TryParse(Console.ReadLine(), out input);
-        while (!check)
-        {
-            Console.WriteLine("The input is wrong! Please input intiger number!");
-            check = int.TryParse(Console.ReadLine(), out input);
-        }
-        return input;
     }
 }
 
