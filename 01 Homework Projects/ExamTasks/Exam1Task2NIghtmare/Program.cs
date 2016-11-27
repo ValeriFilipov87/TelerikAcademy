2961d30f65af6701d908e208512919884dcf5f4e
Valeri Filipov <v.filipov87@gmail.com>
2016-11-27 14:23:39 +0100

    exam1Task2

---
+++ 01 Homework Projects/ExamTasks/Exam1Task2NIghtmare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int isOdd = 0;
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (char.IsDigit(input[i]))
+            {
+                if (isOdd % 2 == 1)
+                {
+                    int digit = Convert.ToInt32(input[i]) - '0';
+                    count++;
+                    sum += digit;
+                }
+            }
+            isOdd++;
+        }
+        Console.WriteLine("{0} {1}", count, sum);
+    }
+}
+
