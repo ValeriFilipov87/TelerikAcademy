c950d19da9760fb28b4f3fa990ff85bb14ba16dc
Valeri Filipov <v.filipov87@gmail.com>
2016-12-20 19:07:14 +0100

    Allocate Array

---
+++ CSharp2/01Arrays/01AllocateArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01AllocateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = new int[20];
+            for (int i = 0; i < digits.Length; i++)
+            {
+                digits[i] = i * 5;
+                Console.Write(digits[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
