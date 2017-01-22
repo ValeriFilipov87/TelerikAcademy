852b6b1cdbcb40e0516fd34199356f362d4e1be6
Valeri Filipov <v.filipov87@gmail.com>
2017-01-22 15:36:46 +0100

    07SelectionSort

---
+++ CSharp2/01Arrays/07SelectionSort/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] digits = input.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            for (int i = 0; i < digits.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < digits.Length; j++)
+                {
+                    if (digits[j] < digits[min])
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    int temp = 0;
+                    temp = digits[i];
+                    digits[i] = digits[min];
+                    digits[min] = temp;               
+                }
+            }
+            for (int i = 0; i < digits.Length; i++)
+            {
+                Console.Write(digits[i]);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
