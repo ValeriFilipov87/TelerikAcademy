3b41f4c850ddc9497e0a1769085f68d1cec70fef
Valeri Filipov <v.filipov87@gmail.com>
2017-02-19 21:06:12 +0100

    task 1 complete

--- CSharp2/02Multidimensional Arrays/ConsoleApplication1/FillTheMatrix.cs
+++ CSharp2/02Multidimensional Arrays/ConsoleApplication1/FillTheMatrix.cs
@@ -19,10 +19,95 @@ namespace ConsoleApplication1
                 case "a": spiral = SolutionA(spiral); PrintSpiral(spiral); break;
                 case "b": spiral = SolutionB(spiral); PrintSpiral(spiral); break;
                 case "c": spiral = SolutionC(spiral); PrintSpiral(spiral); break;
-                case "d": break;
+                case "d": spiral = SolutionD(spiral); PrintSpiral(spiral); break;
             }
         }
 
+        private static int[,] SolutionD(int[,] array)
+        {
+            string direction = "down";
+            int digit = 1;
+            int rowEnd = array.GetLength(0);
+            int colEnd = array.GetLength(1);
+            int rowEE = 0;
+            int colEE = 0;
+            int r = 0;
+            int c = 0;
+
+            while (digit != (array.GetLength(0) * array.GetLength(1) + 1))
+            {
+                if (direction == "down" || direction == "rigth")
+                {
+                    for (int row = r; row < rowEnd; row++)
+                    {
+                        for (int col = c; col < colEnd; col++)
+                        {
+                            if (direction == "down")
+                            {
+                                array[row, col] = digit;
+                                digit++;
+                                if (row + 1 == array.GetLength(0) || array[row + 1, col] != 0)
+                                {
+                                    direction = "rigth";
+                                    r = row;
+                                    c = col + 1;
+                                }
+                                break;
+                            }
+                            if (direction == "rigth")
+                            {
+                                array[row, col] = digit;
+                                digit++;
+                                if (col + 1 == array.GetLength(1) || array[row, col + 1] != 0)
+                                {
+                                    direction = "up";
+                                    c = col;
+                                    r = row - 1;
+                                    rowEnd--;
+                                    colEnd--;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int row = r; row >= rowEE; row--)
+                    {
+                        for (int col = c; col >= colEE; col--)
+                        {
+                            if (direction == "up")
+                            {
+                                array[row, col] = digit;
+                                digit++;
+                                if (row - 1 < 0 || array[row - 1, col] != 0)
+                                {
+                                    direction = "left";
+                                    r = row;
+                                    c = col - 1;
+                                }
+                                break;
+                            }
+                            if (direction == "left")
+                            {
+                                array[row, col] = digit;
+                                digit++;
+                                if (col - 1 < 0 || array[row, col - 1] != 0)
+                                {
+                                    direction = "down";
+                                    c = col;
+                                    r = row + 1;
+                                    rowEE++;
+                                    colEnd++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return array;
+        }
         private static int[,] SolutionC(int[,] array)
         {
             int digit = 1;
@@ -32,7 +117,7 @@ namespace ConsoleApplication1
             for (int row = array.GetLength(0) - 1; row >= 0; row--)
             {
                 for (int col = 0; col < array.GetLength(1); col++)
-                {                    
+                {
                     if (x > array.GetLength(0) - 1)
                     {
                         x = row - 1;
@@ -54,7 +139,7 @@ namespace ConsoleApplication1
                     if (y > array.GetLength(1) - 1)
                     {
                         x = 0;
-                        y = col+1;
+                        y = col + 1;
                         break;
                     }
                     array[x, y] = digit;
@@ -110,11 +195,15 @@ namespace ConsoleApplication1
             {
                 for (int col = 0; col < array.GetLength(1); col++)
                 {
-                    Console.Write("{0,-4}", array[row, col]);
+                    Console.Write("{0, -4}", array[row, col]);
+                    if (col < array.GetLength(1) - 1)
+                    {
+                        Console.Write(" ");
+                    }
+
                 }
                 Console.WriteLine();
             }
         }
-
     }
 }
