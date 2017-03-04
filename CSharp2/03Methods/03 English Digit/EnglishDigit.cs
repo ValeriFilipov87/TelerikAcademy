00843947be9749a395f556933d0bafa3294a2210
Valeri Filipov <v.filipov87@gmail.com>
2017-03-04 10:46:26 +0100

    03 English Digit

---
+++ CSharp2/03Methods/03 English Digit/EnglishDigit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_English_Digit
+{
+    class EnglishDigit
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            PringLastDigit(input);
+        }
+
+        private static void PringLastDigit(int input)
+        {
+            int lastDigit = input % 10;
+            string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            Console.WriteLine(digits[lastDigit]);
+        }
+    }
+}
