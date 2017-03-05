19b1bb4e6ec64f879e8c4237f811d2531a7da6d3
Valeri Filipov <v.filipov87@gmail.com>
2017-03-05 22:30:57 +0100

    07 ReverseNumber

---
+++ CSharp2/03Methods/07 Reverse Number/ReverseNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Reverse_Number
+{
+    class ReverseNumber
+    {
+        public static int arrayLength = new int();
+        public static string arrayInput = "";
+        static void Main(string[] args)
+        {
+            ReadInputData();
+            string[] number = ReverseDigit(arrayInput);
+            PrintAnswer(number);
+        }
+
+        private static void PrintAnswer(string[] number)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write(number[i]);
+            }
+            Console.WriteLine();
+        }
+
+        private static string[] ReverseDigit(string arrayInput)
+        {
+            char[] input = arrayInput.ToCharArray();
+            string[] answer = new string[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                answer[i] = input[input.Length - 1 - i].ToString();
+            }
+            return answer;
+        }
+
+        private static void ReadInputData()
+        {
+            //arrayLength = int.Parse(Console.ReadLine());
+            arrayInput = Console.ReadLine();
+        }
+    }
+}
