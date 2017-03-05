ff771bc388be5406b0df6a425a2ca02e5d9c7378
Valeri Filipov <v.filipov87@gmail.com>
2017-03-05 22:09:02 +0100

    06 Task

---
+++ CSharp2/03Methods/06 First Larger Than Neighbour/FirstLargerThanNeighbour.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_First_Larger_Than_Neighbour
+{
+    class FirstLargerThanNeighbour
+    {
+        public static int arrayLength = new int();
+        public static string arrayInput = "";
+
+        static void Main(string[] args)
+        {
+            ReadInputData();
+            int[] arrayDigits = ConvertStringToIntArray(arrayInput);
+            FindFirstLargerNeighbours(arrayDigits);
+        }
+
+        private static void FindFirstLargerNeighbours(int[] arrayDigits)
+        {
+            bool isThereDigit = false;
+            for (int i = 1; i < arrayDigits.Length - 1; i++)
+            {
+                if (arrayDigits[i] > arrayDigits[i - 1] && arrayDigits[i] > arrayDigits[i + 1])
+                {
+                    Console.WriteLine(i);
+                    isThereDigit = true;
+                    break;
+                }                
+            }
+            if (!isThereDigit)
+            {
+                Console.WriteLine(-1);
+            }
+
+        }
+
+        private static void ReadInputData()
+        {
+            arrayLength = int.Parse(Console.ReadLine());
+            arrayInput = Console.ReadLine();
+        }
+        private static int[] ConvertStringToIntArray(string arrayInput)
+        {
+            string[] tempArray = arrayInput.Split(' ').ToArray();
+            int[] data = new int[tempArray.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = int.Parse(tempArray[i]);
+            }
+            return data;
+        }
+    }
+}
