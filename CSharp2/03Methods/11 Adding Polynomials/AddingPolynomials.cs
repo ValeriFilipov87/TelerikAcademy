5b0fdf0d30dd64cb1f2298d2c5fb42b5da2d394b
Valeri Filipov <v.filipov87@gmail.com>
2017-03-10 22:36:22 +0100

    11 Adding Polynomials

---
+++ CSharp2/03Methods/11 Adding Polynomials/AddingPolynomials.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Adding_Polynomials
+{
+    class AddingPolynomials
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            long[] firstP = ReadInput();
+            long[] secondP = ReadInput();
+            long[] result = AddingP(firstP,secondP,N);
+            PrintResult(result);           
+        }
+
+        private static void PrintResult(long[] result)
+        {
+            string print = string.Join(" ", result);
+            Console.WriteLine(print);
+        }
+
+        private static long[] AddingP(long[] arrayOne, long[] arrayTwo, int length)
+        {
+            long[] result = new long[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = arrayOne[i] + arrayTwo[i];
+            }
+            return result;
+        }
+
+        private static long[] ReadInput()
+        {
+            long[] input = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray();
+            return input;
+        }
+    }
+}
