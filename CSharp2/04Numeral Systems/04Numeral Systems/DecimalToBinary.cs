dd7ccd1ea270e18d74c34debcf9115b935adce4d
Valeri Filipov <v.filipov87@gmail.com>
2017-03-22 22:58:23 +0100

    DecimalToBinary

---
+++ CSharp2/04Numeral Systems/04Numeral Systems/DecimalToBinary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _04Numeral_Systems
+{
+    class DecimalToBinary
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            BigInteger digit = BigInteger.Parse(userInput);
+            string result = DecimalToBinaryConvert(digit);            
+            Console.WriteLine(result);
+        }
+
+        private static string DecimalToBinaryConvert(BigInteger digit)
+        {
+            string tempResult = "";
+            while (digit != 0)
+            {
+                if (digit % 2 == 0)
+                {
+                    tempResult += string.Join("", 0);
+                }
+                else
+                {
+                    tempResult += string.Join("", 1);
+                }
+                digit = digit / 2;
+            }
+            tempResult = StringReverse(tempResult);
+            return tempResult;
+        }
+
+        private static string StringReverse(string result)
+        {
+            char[] temp = result.ToArray();
+            Array.Reverse(temp);
+            return new string(temp);
+        }
+    }
+}
