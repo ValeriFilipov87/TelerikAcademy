40f29eedcca2f84209fd0be94f9ff0195041d6a6
Valeri Filipov <v.filipov87@gmail.com>
2017-03-22 23:25:49 +0100

    0 Binary to Decimal

---
+++ CSharp2/04Numeral Systems/02 Binary To Decimal/BinaryToDecimal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _02_Binary_To_Decimal
+{
+    class BinaryToDecimal
+    {
+        static void Main(string[] args)
+        {
+            string digit = Console.ReadLine();
+            BigInteger result = 0;
+            for (int i = 0; i < digit.Length; i++)
+            {
+                result += (BigInteger)((digit[i] - '0') * Math.Pow(2, digit.Length - 1 - i));
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
