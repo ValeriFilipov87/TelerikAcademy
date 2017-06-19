2fa4ceb51ce7e7a84122d429467206da8d5b9371
Valeri Filipov <v.filipov87@gmail.com>
2017-06-19 22:24:28 +0200

    08_Binary_Short

---
+++ CSharp2/04Numeral Systems/08 Binary Short/BinaryShort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Binary_Short
+{
+    class BinaryShort
+    {
+        static void Main(string[] args)
+        {
+            short digit = short.Parse(Console.ReadLine());
+            if (digit>=0)
+            {
+                Console.WriteLine(ComvertToBinary(digit).PadLeft(16, '0'));
+            }
+            else
+            {
+                short negativeDigit = (short)(Math.Pow(2, 15) + digit);
+                Console.WriteLine(ComvertToBinary(negativeDigit).PadLeft(15, '0').PadLeft(16,'1'));
+            }
+        }
+
+        private static string ComvertToBinary(short digit)
+        {
+            string tempResult = "";
+            while (digit != 0)
+            {
+                if (digit % 2 == 0)
+                {
+                    tempResult += string.Join("", 0);
+                }
+                else
+                {
+                    tempResult += string.Join("", 1);
+                }
+                digit = (short)(digit / 2);
+            }
+            tempResult = StringReverse(tempResult);            
+            return tempResult;
+        }
+
+        private static string StringReverse(string result)
+        {
+            char[] temp = result.ToArray();
+            Array.Reverse(temp);
+            return new string(temp);
+        }
+    }
+}
