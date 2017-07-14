dc8157d3778bcfe1ff43415850386e6a6f3d73f8
Valeri Filipov <v.filipov87@gmail.com>
2017-07-14 17:46:06 +0200

    Problem 3. Day of week

---
+++ CSharp2/05Using Classes and Objects/Problem 3. Day of week/DayOfWeek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3.Day_of_week
+{
+    class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDay = DateTime.Now;
+            Console.WriteLine(currentDay.DayOfWeek);
+        }
+    }
+}
