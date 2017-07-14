ede1f1ccdc57aef770b209f6957b9ae99b0bdc12
Valeri Filipov <v.filipov87@gmail.com>
2017-07-14 17:33:05 +0200

    Problem 1. Leap year

---
+++ CSharp2/05Using Classes and Objects/Problem 1. Leap year/LeapYear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Leap_year
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            int inputYear = int.Parse(Console.ReadLine());
+            DateTime date = new DateTime(inputYear,1,1);
+            bool IsLeapYear = DateTime.IsLeapYear(date.Year);
+            Console.WriteLine(IsLeapYear);
+        }
+    }
+}
