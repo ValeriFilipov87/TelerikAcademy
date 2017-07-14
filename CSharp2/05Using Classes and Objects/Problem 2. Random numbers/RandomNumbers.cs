d0642140cc7ebd28a69df37f2925611d7c3a70e9
Valeri Filipov <v.filipov87@gmail.com>
2017-07-14 17:43:03 +0200

    Problem 2. Random numbers

---
+++ CSharp2/05Using Classes and Objects/Problem 2. Random numbers/RandomNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2.Random_numbers
+{
+    class RandomNumbers
+    {
+        static void Main(string[] args)
+        {
+            Random rndGenerator = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int digit = rndGenerator.Next(100,201);
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
