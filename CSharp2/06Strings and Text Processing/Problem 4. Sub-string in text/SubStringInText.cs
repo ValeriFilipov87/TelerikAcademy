9be5668fbf03b6cac4882ebb72a20df112e43db9
Valeri Filipov <v.filipov87@gmail.com>
2017-07-23 22:26:31 +0200

    Problem 4. Sub-string in text

---
+++ CSharp2/06Strings and Text Processing/Problem 4. Sub-string in text/SubStringInText.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Problem_4.Sub_string_in_text
+{
+    class SubStringInText
+    {
+        static void Main(string[] args)
+        {
+            ////string pattern = Console.ReadLine();
+            ////string input = Console.ReadLine();
+            ////input = input.ToLower();
+            ////int count = 0;
+            ////int result = CountPattern(pattern, input, count);
+            ////Console.WriteLine(result);
+
+            string pattern = Console.ReadLine().ToLower();
+            string input = Console.ReadLine().ToLower();
+            int curIndex = input.IndexOf(pattern);
+            int counter = 0;
+
+            while (curIndex >= 0)
+            {
+                counter++;
+                curIndex = input.IndexOf(pattern, ++curIndex);
+            }
+
+            Console.WriteLine(counter);
+
+        }
+
+        //private static int CountPattern(string pattern, string input, int patternCount)
+        //{
+        //    if (!input.Contains(pattern))
+        //    {
+        //        return patternCount;
+        //    }
+        //    else
+        //    {
+        //        int index = input.IndexOf(pattern);
+        //        input = input.Remove(index, pattern.Length);
+        //        patternCount = CountPattern(pattern, input, patternCount + 1);
+        //    }
+        //    return patternCount;
+        //}
+    }
+}
