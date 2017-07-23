a1040530a1aed67d3a044b2e501093ca5869f955
Valeri Filipov <v.filipov87@gmail.com>
2017-07-23 23:38:34 +0200

    Problem 6. String length

---
+++ CSharp2/06Strings and Text Processing/Problem 6. String length/StringLength.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6.String_length
+{
+    class StringLength
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int length = input.Length;
+
+            if (length < 20)
+            {
+                input = input.PadRight(20, '*');
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
