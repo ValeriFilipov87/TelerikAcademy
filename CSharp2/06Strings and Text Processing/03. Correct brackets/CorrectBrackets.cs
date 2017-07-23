d5da4624ae8103ffe3532bb68c4ca21575a2af33
Valeri Filipov <v.filipov87@gmail.com>
2017-07-23 12:08:46 +0200

    Problem 3. Correct brackets

---
+++ CSharp2/06Strings and Text Processing/03. Correct brackets/CorrectBrackets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Correct_brackets
+{
+    class CorrectBrackets
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<string> bracketStatus = new Stack<string>();
+            string status = "Correct";
+            int countOpen = 0;
+            int countClosed = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    countOpen++;
+                    bracketStatus.Push("Open");
+                }
+                if (input[i] == ')')
+                {
+                    countClosed++;
+                    if (bracketStatus.Count > 0)
+                    {
+                        bracketStatus.Pop();
+                    }
+                    else
+                    {
+                        status = "Incorrect";
+                        break;
+                    }
+                }
+            }
+            if (countOpen != countClosed)
+            {
+                status = "Incorrect";
+            }
+            Console.WriteLine(status);
+        }
+    }
+}
