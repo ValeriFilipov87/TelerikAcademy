8bc4e07f3f0155b1a222c114c407b2303a5b0170
Valeri Filipov <v.filipov87@gmail.com>
2017-07-23 23:30:38 +0200

    Problem 5. Parse tags

---
+++ CSharp2/06Strings and Text Processing/Problem 5. Parse tags/ParseTags.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_5.Parse_tags
+{
+    class ParseTags
+    {
+        static void Main(string[] args)
+        {
+            string startTag = "<upcase>";
+            string endTag = "</upcase>";
+            StringBuilder result = new StringBuilder();
+            string input = Console.ReadLine();
+            string toUpper = "";
+            
+            int indexEnd = input.IndexOf(endTag);
+
+            while (indexEnd >= 0)
+            {
+                result.Append(input.Substring(0, input.IndexOf(startTag)));
+                input = input.Remove(0, input.IndexOf(startTag) + startTag.Length);
+                toUpper = input.Substring(0, input.IndexOf(endTag)).ToUpper().ToString();
+                result.Append(toUpper);
+                input = input.Remove(0, input.IndexOf(endTag) + endTag.Length);
+                indexEnd = input.IndexOf(endTag);
+            }
+            result.Append(input);
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
