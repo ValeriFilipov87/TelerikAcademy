e5552234eeef8b6861ff0603af20dae0c7c475b2
Valeri Filipov <v.filipov87@gmail.com>
2017-07-23 10:51:43 +0200

    02. Reverse string

---
+++ CSharp2/06Strings and Text Processing/Problem 2. Reverse string/ReverseString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2.Reverse_string
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = StringReverse(input);
+            Console.WriteLine(input);
+        }
+
+        private static string StringReverse(string input)
+        {
+            StringBuilder temp = new StringBuilder();
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                temp.Append(input[i]);
+            }
+            input = temp.ToString();
+            return input;
+        }
+    }
+}
