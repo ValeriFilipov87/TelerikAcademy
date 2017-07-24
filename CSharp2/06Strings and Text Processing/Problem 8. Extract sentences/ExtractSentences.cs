608c1c1510f45b2f5d9b3662eae4d5c23b3dd4db
Valeri Filipov <v.filipov87@gmail.com>
2017-07-24 19:50:57 +0200

    Problem 8. Extract sentences

---
+++ CSharp2/06Strings and Text Processing/Problem 8. Extract sentences/ExtractSentences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_8.Extract_sentences
+{
+    class ExtractSentences
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string sentence = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            string[] text = sentence.Split('.').Select(x => x.Trim()).ToArray();
+            char[] separators = sentence.Where(x => !char.IsLetter(x)).Distinct().ToArray();
+
+            foreach (string item in text)
+            {
+                string[] sentenceWords = item.Split(separators).ToArray();
+
+                for (int i = 0; i < sentenceWords.Length; i++)
+                {
+                    if (sentenceWords[i] == word)
+                    {
+                        string temp = item + ". ";
+                        result.Append(temp);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(result.ToString().Trim());
+
+        }
+    }
+}
