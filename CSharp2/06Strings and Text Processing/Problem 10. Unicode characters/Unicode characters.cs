64a54da52f0cfa3f6e583742b3fe28f2c18788ce
Valeri Filipov <v.filipov87@gmail.com>
2017-08-25 12:57:48 +0200

    Problem 10. Unicode characters

---
+++ CSharp2/06Strings and Text Processing/Problem 10. Unicode characters/Unicode characters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_10.Unicode_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+            
+            foreach (var item in input)
+            {
+                result.Append(GetEscapeSequence(item));
+            }
+
+            string print = result.ToString();
+            Console.WriteLine(print);
+        }
+
+        static string GetEscapeSequence(char c)
+        {
+            return "\\u" + ((int)c).ToString("X4");
+        }
+    }
+}
