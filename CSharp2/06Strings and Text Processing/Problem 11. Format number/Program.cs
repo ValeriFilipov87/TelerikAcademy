6c490e7017f1364800381a2ecb88644e87f5caaa
Valeri Filipov <v.filipov87@gmail.com>
2017-08-25 13:25:01 +0200

    Problem 11. Format number

---
+++ CSharp2/06Strings and Text Processing/Problem 11. Format number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_11.Format_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}",input);
+            Console.WriteLine("{0:X}", input);
+            Console.WriteLine("{0:P2}", input);
+            Console.WriteLine("{0:E2}", input);
+        }
+    }
+}
