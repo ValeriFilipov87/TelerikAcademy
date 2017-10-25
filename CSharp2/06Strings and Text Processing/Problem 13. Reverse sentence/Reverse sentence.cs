4cabb15a52c5eb763b7aae294e04d075150bd3fb
Valeri Filipov <v.filipov87@gmail.com>
2017-10-25 23:54:02 +0200

    Problem 13. Reverse sentence

---
+++ CSharp2/06Strings and Text Processing/Problem 13. Reverse sentence/Reverse sentence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_13.Reverse_sentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] signs = { ',', '.', '!', '?', ' '};
+            string[] temp = input.Split(' ').Select(x => x.Trim()).ToArray();
+
+            string[] words = input.Split(signs).Select(x => x.Trim()).ToArray();
+            words = words.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            Array.Reverse(words);
+
+            for (int j = 0; j < signs.Length-1; j++)
+            {
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    int index = temp[i].IndexOf(signs[j]);
+                    if (index > 0)
+                    {
+                        words[i] += signs[j];
+                        index = -1;
+                    }
+                }
+            }          
+            string result = string.Join(" ", words);
+            Console.WriteLine(result);
+        }
+    }
+}
