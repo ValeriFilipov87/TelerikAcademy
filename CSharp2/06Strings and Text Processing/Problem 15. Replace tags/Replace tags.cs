d2b0ca9575856cf43bd20c48ee7554c5db5131eb
Valeri Filipov <v.filipov87@gmail.com>
2017-10-29 13:02:38 +0100

    Problem 15. Replace tags

---
+++ CSharp2/06Strings and Text Processing/Problem 15. Replace tags/Replace tags.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace_tags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string tempImput = input;
+            string startTag = "<a href=\"";
+            string middleTag = "\">";
+            string endTag = "</a>";
+
+            do
+            {
+                string address = tempImput.Substring(tempImput.IndexOf(startTag) + startTag.Length, tempImput.IndexOf(middleTag) - tempImput.IndexOf(startTag) - startTag.Length);
+                string text = tempImput.Substring(tempImput.IndexOf(middleTag) + middleTag.Length, tempImput.IndexOf(endTag) - tempImput.IndexOf(middleTag) - middleTag.Length);
+                tempImput = tempImput.Substring(tempImput.IndexOf(endTag) + endTag.Length);
+                input = input.Replace(startTag + address + middleTag + text + endTag, "[" + text + "]" + "(" + address + ")");
+            } while (tempImput.IndexOf(endTag) != -1);
+
+            Console.WriteLine(input);
+        }
+    }
+}
