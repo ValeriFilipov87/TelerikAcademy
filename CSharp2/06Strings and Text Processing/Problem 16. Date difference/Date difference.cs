f5451d90695c00d78fe2afaacddaae28646e31cb
Valeri Filipov <v.filipov87@gmail.com>
2017-10-29 13:15:07 +0100

    Problem 16. Date difference

---
+++ CSharp2/06Strings and Text Processing/Problem 16. Date difference/Date difference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_16.Date_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime inputFirstDate = Convert.ToDateTime(Console.ReadLine());
+            DateTime inputSecondDate = Convert.ToDateTime(Console.ReadLine());
+
+            int result = (inputSecondDate - inputFirstDate).Days;
+            Console.WriteLine(result);
+        }
+    }
+}
