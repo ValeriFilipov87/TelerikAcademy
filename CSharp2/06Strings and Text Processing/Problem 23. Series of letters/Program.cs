92725df1e7191197200fdbffe6a88ac4a0e1c229
Valeri Filipov <v.filipov87@gmail.com>
2017-10-29 14:58:24 +0100

    Problem_23.Series_of_letters

---
+++ CSharp2/06Strings and Text Processing/Problem 23. Series of letters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_23.Series_of_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int countOfRepeatedChars = 0;
+            string result = input;
+
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    result = result.Remove(countOfRepeatedChars,1);
+                }
+                else
+                {
+                    countOfRepeatedChars++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
