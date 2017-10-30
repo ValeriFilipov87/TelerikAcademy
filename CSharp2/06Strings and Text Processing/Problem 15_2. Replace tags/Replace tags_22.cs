53ef61ecc4bd158a87c44b48a6769fc2899b911a
Valeri Filipov <v.filipov87@gmail.com>
2017-10-30 08:27:54 +0100

    Problem_15_2.Replace_tags

---
+++ CSharp2/06Strings and Text Processing/Problem 15_2. Replace tags/Replace tags_22.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_15_2.Replace_tags
+{
+    class ReplaceTags
+    {
+        static void Main(string[] args)
+        {
+            string startTag = @"<a ";
+            string endTag = @"</a>";
+            string[] input = Console.ReadLine().Split(new[] { startTag, endTag }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string result = "";
+
+            for (int index = 0; index < input.Length; index++)
+            {
+                if (input[index].IndexOf("href=") == 0)
+                {
+                    string url = GetUrl(input[index]);
+                    string label = GetLabel(input[index]);
+                    Console.Write("[{0}] ({1})", label, url);
+                }
+                else
+                {
+                    Console.Write(input[index]);
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        private static string GetLabel(string label)
+        {
+            int left = label.IndexOf(">");
+            int right = label.IndexOf("<", left + 1);
+
+            return label.Substring(left + 1);
+        }
+
+        private static string GetUrl(string url)
+        {
+            int left = url.IndexOf("\"");
+            int right = url.IndexOf("\"", left + 1);
+
+            return url.Substring(left + 1, right - left - 1);
+        }
+    }
+}
---
+++ CSharp2/07Exception Handling/Problem 1. Square root/Square root.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Square_root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+           // double digit = ConvertToDigit(input);
+            //double result = Math.Sqrt(digit);
+            try
+            {
+                double digit = ConvertToDigit(input);
+                double result = Math.Sqrt(digit);
+                Console.WriteLine("{0:F3}", result);
+            }            
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+            
+           // Console.WriteLine(result);
+        }
+
+        private static double ConvertToDigit(string input)
+        {
+            double result = new int();
+            try
+            {
+                double digit = double.Parse(input);
+                result = digit;
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException("Invalid number");
+            }
+
+            if (result <0)
+            {
+                throw new ArgumentException("Invalid number");
+            }
+            return result;
+        }
+    }
+}
